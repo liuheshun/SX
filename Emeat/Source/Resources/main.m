b218b7a2a8306a11645372fe01d5eb4a1e00ac76
liuheshun <w@liuheshundeMacBook-Pro.local>
2018-03-26 14:33:45 +0800

    资源文件

---
+++ Emeat/Source/Resources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Emeat
+//
+//  Created by liuheshun on 2017/11/3.
+//  Copyright © 2017年 liuheshun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
