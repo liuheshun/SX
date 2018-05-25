2ef4332631e283ba5d118e128a98ee5b4a140a75
liuheshun <w@liuheshundeMacBook-Pro.local>
2018-05-25 14:08:34 +0800

    添加消息

---
+++ Emeat/Source/CodeClass/PushMessage/MessageCenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessageCenterViewController.h
+//  Emeat
+//
+//  Created by liuheshun on 2018/5/25.
+//  Copyright © 2018年 liuheshun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageCenterViewController : UIViewController
+
+@end
---
+++ Emeat/Source/CodeClass/PushMessage/MessageCenterViewController.m
@@ -0,0 +1,38 @@
+//
+//  MessageCenterViewController.m
+//  Emeat
+//
+//  Created by liuheshun on 2018/5/25.
+//  Copyright © 2018年 liuheshun. All rights reserved.
+//
+
+#import "MessageCenterViewController.h"
+
+@interface MessageCenterViewController ()
+
+@end
+
+@implementation MessageCenterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
