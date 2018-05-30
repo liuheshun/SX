b107a2ee73f125480b37f519750e2e3cce0be9ca
liuheshun <w@liuheshundeMacBook-Pro.local>
2018-05-30 16:49:27 +0800

    0530-admim 1.3

--- Emeat/Source/Macro/URLHeader.h
+++ Emeat/Source/Macro/URLHeader.h
@@ -14,10 +14,10 @@
 #define appStoreURL @"https://itunes.apple.com/cn/app/%E8%B5%9B%E9%B2%9C/id1364356601?mt=8"
 
 ////URL:
-#define baseUrl @"http://beta.cyberfresh.cn/m"
-#define loginBaseUrl @"http://beta.cyberfresh.cn/cas"
+#define baseUrl @"http://admin.cyberfresh.cn/m"
+#define loginBaseUrl @"http://admin.cyberfresh.cn/cas"
 ///设置的
-#define setbaseUrl @"http://beta.cyberfresh.cn"
+#define setbaseUrl @"http://admin.cyberfresh.cn"
 
 
 //
